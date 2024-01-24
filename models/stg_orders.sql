eb79ba0c4b770ff8ca4ce241a90a922c23719c70
Jing Yu Lim <jingyu@infinitelambda.com>
2024-01-24 03:12:06 +0000

    Update stg models based on new sources

--- models/stg_orders.sql
+++ models/stg_orders.sql
@@ -1,20 +1,9 @@
 with orders as (
-    select * from {{ source('snowflake_sample', 'orders') }}
+    select * from {{ source('snowflake_sample', 'raw_orders') }}
 )
 
 , final as (
-    select 
-        o_orderkey as order_key
-        , o_custkey as customer_key
-        , o_orderstatus as order_status
-        , o_totalprice as total_price
-        , o_orderdate as order_date 
-        , o_orderpriority as order_priority
-        , o_clerk as clerk 
-        , o_shippriority as ship_priority
-        , o_comment as comment
- 
-    from orders
+    select * from orders
 )
 
 select * from final

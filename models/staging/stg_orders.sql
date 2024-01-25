110ecedec26d2d2dec81bc3c6127e6614ef68268
Jing Yu Lim <jingyu@infinitelambda.com>
2024-01-25 02:50:23 +0000

    Update existing staging tables

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -3,7 +3,13 @@ with orders as (
 )
 
 , final as (
-    select * exclude(_row) from orders
+    select 
+        id as order_id
+        , user_id as customer_id
+        , status as order_status
+        , order_date
+    
+    from orders
 )
 
 select * from final

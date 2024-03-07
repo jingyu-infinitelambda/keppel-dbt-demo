80892bdcbd0b1f4653ec81f9007173264bd29763
Jing Yu Lim <jingyu@infinitelambda.com>
2024-03-07 04:12:15 +0000

    Refactor step 5

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -12,79 +12,92 @@ with orders as (
 )
 
 -- logical CTE
-, customers_name as (
+
+-- staging customers
+, stg_customers as (
     select
-        *
-        , first_name || ' ' || last_name as name
+        id as customer_id
+        , last_name
+        , first_name 
+        , first_name || ' ' || last_name as full_name
     from customers
 )
 
-, a as (
+-- staging orders
+, stg_orders as (
     select
-        *
+        id as order_id
+        , user_id as customer_id
+        , status as order_status
+        , order_date
         , row_number()
             over (partition by user_id order by order_date, id)
             as user_order_seq
     from orders    
 )
 
-, b as (
-    select
-        *
-        , first_name || ' ' || last_name as name
-    from customers
+-- staging payments 
+, stg_payments as (
+    select 
+        id as payment_id
+        , payment_method
+        , order_id 
+        , round(amount / 100.0, 2) as order_value_dollars
+
+    from payments 
 )
 
 , customer_order_history as (
     select
-        b.id as customer_id
-        , b.name as full_name
-        , b.last_name as surname
-        , b.first_name as givenname
+        stg_customers.customer_id
+        , stg_customers.full_name
+        , stg_customers.last_name 
+        , stg_customers.first_name
         , min(order_date) as first_order_date
-        , min(case when a.status not in ('returned', 'return_pending') then order_date end) as first_non_returned_order_date
-        , max(case when a.status not in ('returned', 'return_pending') then order_date end) as most_recent_non_returned_order_date
+        , min(case when stg_orders.order_status not in ('returned', 'return_pending') then order_date end) as first_non_returned_order_date
+        , max(case when stg_orders.order_status not in ('returned', 'return_pending') then order_date end) as most_recent_non_returned_order_date
         , coalesce(max(user_order_seq), 0) as order_count
-        , coalesce(count(case when a.status != 'returned' then 1 end), 0) as non_returned_order_count
-        , sum(case when a.status not in ('returned', 'return_pending') then round(c.amount / 100.0, 2) else 0 end) as total_lifetime_value
-        , sum(case when a.status not in ('returned', 'return_pending') then round(c.amount / 100.0, 2) else 0 end) / nullif(count(case when a.status not in ('returned', 'return_pending') then 1 end), 0) as avg_non_returned_order_value
-        , array_agg(distinct a.id) as order_ids
+        , coalesce(count(case when stg_orders.order_status != 'returned' then 1 end), 0) as non_returned_order_count
+        , sum(case when stg_orders.order_status not in ('returned', 'return_pending') then stg_payments.order_value_dollars else 0 end) as total_lifetime_value
+        , sum(case when stg_orders.order_status not in ('returned', 'return_pending') then stg_payments.order_value_dollars else 0 end) / nullif(count(case when stg_orders.order_status not in ('returned', 'return_pending') then 1 end), 0) as avg_non_returned_order_value
+        , array_agg(distinct stg_orders.order_id) as order_ids
 
-    from a
+    from stg_orders
 
-    inner join b
-        on a.user_id = b.id
+    inner join stg_customers
+        on stg_orders.customer_id = stg_customers.customer_id
 
-    left outer join payments as c
-        on a.id = c.order_id
+    left outer join stg_payments
+        on stg_orders.order_id = stg_payments.order_id
 
-    where a.status not in ('pending')
+    where stg_orders.order_status not in ('pending')
 
-    group by b.id, b.name, b.last_name, b.first_name
+    group by stg_customers.customer_id, stg_customers.full_name, stg_customers.last_name, stg_customers.first_name
 )
 
 -- final CTE
 , final as (
     select
-        orders.id as order_id
-        , orders.user_id as customer_id
-        , last_name as surname
-        , first_name as givenname
+        stg_orders.order_id
+        , stg_orders.customer_id
+        , stg_customers.last_name 
+        , stg_customers.first_name
         , first_order_date
         , order_count
         , total_lifetime_value
-        , orders.status as order_status
-        , payments.payment_method
-        , round(amount / 100.0, 2) as order_value_dollars
-    from orders 
-    inner join customers_name
-        on orders.user_id = customers_name.id
+        , stg_orders.order_status
+        , stg_payments.payment_id
+        , stg_payments.payment_method
+        , stg_payments.order_value_dollars
+    from stg_orders 
+    inner join stg_customers
+        on stg_orders.customer_id = stg_customers.customer_id
 
     inner join customer_order_history
-        on orders.user_id = customer_order_history.customer_id
+        on stg_orders.customer_id = customer_order_history.customer_id
 
-    left outer join payments
-        on orders.id = payments.order_id
+    left outer join stg_payments
+        on stg_orders.order_id = stg_payments.order_id
 )
 
 -- final select statment

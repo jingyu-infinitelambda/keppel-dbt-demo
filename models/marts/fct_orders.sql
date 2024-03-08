4587d03ab24d02fd65b73c24ecb84c07924bba9a
Jing Yu Lim <jingyu@infinitelambda.com>
2024-03-08 07:13:53 +0000

    Add is_incremental block

--- models/marts/fct_orders.sql
+++ models/marts/fct_orders.sql
@@ -7,6 +7,10 @@
 
 with orders as (
     select * from {{ ref('stg_orders') }}
+    {% if is_incremental() %}
+        -- this filter will only be applied on an incremental run
+        where order_date > (select max(order_date) from {{ this }}) 
+    {% endif %}
 )
 
 , customers as (

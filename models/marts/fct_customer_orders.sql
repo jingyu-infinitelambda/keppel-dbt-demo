314ccb05e30120cebd3862ddc3bb7ec175ea8969
Jing Yu Lim <jingyu@infinitelambda.com>
2024-03-18 06:46:40 +0000

    Reorganise CTEs

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -1,28 +1,38 @@
-select
-    orders.id as order_id,
-    orders.user_id as customer_id,
-    customers.last_name,
-    customers.first_name,
-    first_order_date,
-    order_count,
-    total_lifetime_value,
-    round(amount / 100.0, 2) as order_value_dollars,
-    orders.status as order_status,
-    payments.id as payment_id,
-    payments.payment_method
-from {{ source("snowflake_sample", "raw_orders") }} as orders
+-- import CTE
+with base_orders as (
+    select * from {{ source("snowflake_sample", "raw_orders") }}
+),
 
-join
-    (
+base_customers as (
+    select * from {{ source("snowflake_sample", "raw_customers") }}
+),
+
+base_payments as (
+    select * from {{ source("snowflake_sample", "raw_payments") }}
+),
+
+-- logical CTEs
+customers as (
         select first_name || ' ' || last_name as name, *
-        from {{ source("snowflake_sample", "raw_customers") }}
-    ) customers
-    on orders.user_id = customers.id
+        from base_customers    
+),
 
-join
-    (
-
+a as (
         select
+            row_number() over (
+                partition by user_id order by order_date, id
+            ) as user_order_seq,
+            *
+        from base_orders
+),
+
+b as (
+                    select first_name || ' ' || last_name as name, *
+                from base_customers
+),
+
+customer_order_history as (
+            select
             b.id as customer_id,
             b.name as full_name,
             b.last_name,
@@ -63,33 +73,44 @@ join
             ) as avg_non_returned_order_value,
             array_agg(distinct a.id) as order_ids
 
-        from
-            (
-                select
-                    row_number() over (
-                        partition by user_id order by order_date, id
-                    ) as user_order_seq,
-                    *
-                from {{ source("snowflake_sample", "raw_orders") }}
-            ) a
+        from a
 
-        join
-            (
-                select first_name || ' ' || last_name as name, *
-                from {{ source("snowflake_sample", "raw_customers") }}
-            ) b
+        join b
             on a.user_id = b.id
 
         left outer join
-            {{ source("snowflake_sample", "raw_payments") }} c on a.id = c.order_id
+            base_payments c on a.id = c.order_id
 
         where a.status not in ('pending')
 
         group by b.id, b.name, b.last_name, b.first_name
+),
 
-    ) customer_order_history
-    on orders.user_id = customer_order_history.customer_id
+-- final CTE
+final as (
+    select
+        orders.id as order_id,
+        orders.user_id as customer_id,
+        customers.last_name,
+        customers.first_name,
+        first_order_date,
+        order_count,
+        total_lifetime_value,
+        round(amount / 100.0, 2) as order_value_dollars,
+        orders.status as order_status,
+        payments.id as payment_id,
+        payments.payment_method
+    from base_orders as orders
 
-left outer join
-    {{ source("snowflake_sample", "raw_payments") }} payments
-    on orders.id = payments.order_id
+    join customers
+        on orders.user_id = customers.id
+
+    join customer_order_history
+        on orders.user_id = customer_order_history.customer_id
+
+    left outer join
+        base_payments payments
+        on orders.id = payments.order_id
+)
+
+select * from final

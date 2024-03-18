6062bbfc0a7da53ff90745055bd975a29dc0c352
Jing Yu Lim <jingyu@infinitelambda.com>
2024-03-18 06:40:01 +0000

    Step 4 - Format SQL

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -1,4 +1,4 @@
-select 
+select
     orders.id as order_id,
     orders.user_id as customer_id,
     customers.last_name,
@@ -6,60 +6,90 @@ select
     first_order_date,
     order_count,
     total_lifetime_value,
-    round(amount/100.0,2) as order_value_dollars,
+    round(amount / 100.0, 2) as order_value_dollars,
     orders.status as order_status,
     payments.id as payment_id,
     payments.payment_method
-from {{ source('snowflake_sample', 'raw_orders') }} as orders
+from {{ source("snowflake_sample", "raw_orders") }} as orders
 
-join (
-      select 
-        first_name || ' ' || last_name as name, 
-        * 
-      from {{ source('snowflake_sample', 'raw_customers') }}
-) customers
-on orders.user_id = customers.id
+join
+    (
+        select first_name || ' ' || last_name as name, *
+        from {{ source("snowflake_sample", "raw_customers") }}
+    ) customers
+    on orders.user_id = customers.id
 
-join (
+join
+    (
 
-    select 
-        b.id as customer_id,
-        b.name as full_name,
-        b.last_name,
-        b.first_name,
-        min(order_date) as first_order_date,
-        min(case when a.status NOT IN ('returned','return_pending') then order_date end) as first_non_returned_order_date,
-        max(case when a.status NOT IN ('returned','return_pending') then order_date end) as most_recent_non_returned_order_date,
-        COALESCE(max(user_order_seq),0) as order_count,
-        COALESCE(count(case when a.status != 'returned' then 1 end),0) as non_returned_order_count,
-        sum(case when a.status NOT IN ('returned','return_pending') then ROUND(c.amount/100.0,2) else 0 end) as total_lifetime_value,
-        sum(case when a.status NOT IN ('returned','return_pending') then ROUND(c.amount/100.0,2) else 0 end)/NULLIF(count(case when a.status NOT IN ('returned','return_pending') then 1 end),0) as avg_non_returned_order_value,
-        array_agg(distinct a.id) as order_ids
+        select
+            b.id as customer_id,
+            b.name as full_name,
+            b.last_name,
+            b.first_name,
+            min(order_date) as first_order_date,
+            min(
+                case
+                    when a.status not in ('returned', 'return_pending') then order_date
+                end
+            ) as first_non_returned_order_date,
+            max(
+                case
+                    when a.status not in ('returned', 'return_pending') then order_date
+                end
+            ) as most_recent_non_returned_order_date,
+            coalesce(max(user_order_seq), 0) as order_count,
+            coalesce(
+                count(case when a.status != 'returned' then 1 end), 0
+            ) as non_returned_order_count,
+            sum(
+                case
+                    when a.status not in ('returned', 'return_pending')
+                    then round(c.amount / 100.0, 2)
+                    else 0
+                end
+            ) as total_lifetime_value,
+            sum(
+                case
+                    when a.status not in ('returned', 'return_pending')
+                    then round(c.amount / 100.0, 2)
+                    else 0
+                end
+            ) / nullif(
+                count(
+                    case when a.status not in ('returned', 'return_pending') then 1 end
+                ),
+                0
+            ) as avg_non_returned_order_value,
+            array_agg(distinct a.id) as order_ids
 
-    from (
-      select 
-        row_number() over (partition by user_id order by order_date, id) as user_order_seq,
-        *
-      from {{ source('snowflake_sample', 'raw_orders') }}
-    ) a
+        from
+            (
+                select
+                    row_number() over (
+                        partition by user_id order by order_date, id
+                    ) as user_order_seq,
+                    *
+                from {{ source("snowflake_sample", "raw_orders") }}
+            ) a
 
-    join ( 
-      select 
-        first_name || ' ' || last_name as name, 
-        * 
-      from {{ source('snowflake_sample', 'raw_customers') }}
-    ) b
-    on a.user_id = b.id
+        join
+            (
+                select first_name || ' ' || last_name as name, *
+                from {{ source("snowflake_sample", "raw_customers") }}
+            ) b
+            on a.user_id = b.id
 
-    left outer join {{ source('snowflake_sample', 'raw_payments') }} c
-    on a.id = c.order_id
+        left outer join
+            {{ source("snowflake_sample", "raw_payments") }} c on a.id = c.order_id
 
-    where a.status NOT IN ('pending') 
+        where a.status not in ('pending')
 
-    group by b.id, b.name, b.last_name, b.first_name
+        group by b.id, b.name, b.last_name, b.first_name
 
-) customer_order_history
-on orders.user_id = customer_order_history.customer_id
+    ) customer_order_history
+    on orders.user_id = customer_order_history.customer_id
 
-left outer join {{ source('snowflake_sample', 'raw_payments') }} payments
-on orders.id = payments.order_id
+left outer join
+    {{ source("snowflake_sample", "raw_payments") }} payments
+    on orders.id = payments.order_id

b3143db90686e7560e8511d8ea5f3a66ffa98bfd
dat nguyen <dat@infinitelambda.com>
2024-03-21 04:36:30 +0000

    Added semantic models + metrics

---
+++ models/marts/dim_customers.sql
@@ -0,0 +1,25 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='customer_id'
+    )
+}}
+
+with customers as (
+    select * from {{ ref('stg_customers') }}
+
+    {% if is_incremental() %}
+        where customer_id in (select customer_id from {{ this }}) 
+    {% endif %}
+)
+
+, final as (
+    select
+        customers.customer_id
+        , customers.first_name
+        , customers.last_name
+
+    from customers
+)
+
+select * from final

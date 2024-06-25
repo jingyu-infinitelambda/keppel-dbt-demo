5a84259e47128fd800e83c07d95eac5b547256ae
Jing Yu Lim <jingyu@infinitelambda.com>
2024-06-25 08:18:38 +0000

    Add yml for unit tests

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -6,7 +6,10 @@ with orders as (
     select 
         id as order_id
         , user_id as customer_id
-        , status as order_status
+        , case 
+            when status like '%return%' then 'return'
+            else status  
+          end as order_status
         , order_date
         , row_number() over (
                 partition by user_id order by order_date, id
